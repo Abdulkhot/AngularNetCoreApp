392bba1346673979a4af453677dab8860ed94ab1
Abdulkhot <66285786+Abdulkhot@users.noreply.github.com>
2024-02-12 20:39:01 +0530

    Comit

---
+++ AngularApplication.API/AngularApplication.API/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularApplication.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<students>>> GetStudents()
+        {
+            return new List<students>
+            {
+                new students
+                {
+                    firstName = "Abdulhamid 1",
+                    lastName = "Khot 1",
+                    place = "Mumbai 1"
+                }
+            };
+        }
+    }
+}
---
+++ AngularApplication.API/AngularApplication.API/students.cs
@@ -0,0 +1,11 @@
+﻿namespace AngularApplication.API
+{
+    public class students
+    {
+        public int id { get; set; }
+        public string firstName { get; set; } = String.Empty;
+        public string lastName { get; set; } = String.Empty;
+        public string place { get; set; } = String.Empty;
+        
+    }
+}
